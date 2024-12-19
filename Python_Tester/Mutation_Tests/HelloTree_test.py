c5ec242244bd126bf56d027214e2052c5dd78331
Jermaine L Brown <78555319+brownman5@users.noreply.github.com>
2024-12-19 17:38:39 -0500

    Refactored HelloTree_test cases into more easily discernable cases. Pair Programming Partner: Chris #18

--- Python_Tester/Mutation_Tests/HelloTree_test.py
+++ Python_Tester/Mutation_Tests/HelloTree_test.py
@@ -8,43 +8,55 @@ sys.path.append(parent)
 from Mutator import HelloTree
 from Original_Files.HelloCode_Tester import HelloWorld_test as Test
 
+test_tree = None
+
 class MyTestCase(unittest.TestCase):
         def test_startup(self):
-                test_tree = HelloTree.HelloTree()
-                self.assertIsNotNone(test_tree)
-                test_tree.traverseTree()
-                self.assertEqual(test_tree.retOperations(), ["+", "+", "+"])
-                self.assertEqual(test_tree.retVariables(), ["testArray", "rM", "rM"])
-                self.assertEqual(test_tree.retValues(), [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 40, 60, 'x', "Wowzers!", '178'])
-                self.assertTrue(Test.MyTestCase())
-                test_tree.basicMutateTree()
-                test_tree.loadMutatedCode(0)
-                try:
-                       self.assertFalse(Test.MyTestCase()) ##When running mutated code change this to assertFalse
-                except:
-                       print("Test case crashed!")
-                test_tree.loadOriginalCode()
-                with open(parent + '/Original_Files/HelloCode/HelloWorld.py', 'r') as fd:
-                       code = fd.read()
-                       self.assertEqual(code, test_tree.original_code) 
+              self.test_tree = HelloTree.HelloTree()
+              self.assertIsNotNone(self.test_tree)
+              self.assertTrue(Test.MyTestCase())
+        
+        def test_original_tree(self): 
+              self.test_startup()
 
-        def test_mutation_variations(self):
-              test_tree = HelloTree.HelloTree()
-              self.assertIsNotNone(test_tree)
-              test_tree.basicMutateTree()
-              for i in range(test_tree.retMutationLength()):
-                     test_tree.loadMutatedCode(i)
+              self.test_tree.traverseTree()
+              self.assertEqual(self.test_tree.retOperations(), ["+", "+", "+"])
+              self.assertEqual(self.test_tree.retVariables(), ["testArray", "rM", "rM"])
+              self.assertEqual(self.test_tree.retValues(), [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 40, 60, 'x', "Wowzers!", '178'])
+
+        def test_first_mutation(self):
+              self.test_startup()
+
+              self.test_tree.basicMutateTree()
+              self.test_tree.loadMutatedCode(0)
+              try:
+                     self.assertFalse(Test.MyTestCase()) ##When running mutated code change this to assertFalse
+              except:
+                     print("Test case crashed!")
+              self.test_tree.loadOriginalCode()
+              with open(parent + '/Original_Files/HelloCode/HelloWorld.py', 'r') as fd:
+                     code = fd.read()
+                     self.assertEqual(code, self.test_tree.original_code) 
+
+        def test_all_mutation_variations(self):
+              self.test_tree = HelloTree.HelloTree()
+              self.assertIsNotNone(self.test_tree)
+              self.test_tree.basicMutateTree()
+              for i in range(self.test_tree.retMutationLength()):
+                     self.test_tree.loadMutatedCode(i)
                      if (i == 0):
-                            self.assertEqual(test_tree.retOperations(), ["-", "+", "+"])
+                            self.assertEqual(self.test_tree.retOperations(), ["-", "+", "+"])
                      if (i == 1):
-                            self.assertEqual(test_tree.retOperations(), ["+", "-", "+"])
+                            self.assertEqual(self.test_tree.retOperations(), ["+", "-", "+"])
                      if (i == 2):
-                            self.assertEqual(test_tree.retOperations(), ["+", "+", "-"])
+                            self.assertEqual(self.test_tree.retOperations(), ["+", "+", "-"])
                      try:
                        self.assertFalse(Test.MyTestCase()) ##When running mutated code change this to assertFalse
                      except:
                        print("Test case crashed!")
-                     test_tree.loadOriginalCode()
+                     self.test_tree.loadOriginalCode()
+
+        
 
                 
 
