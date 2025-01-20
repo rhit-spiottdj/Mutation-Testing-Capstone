793673bcf58d122df066be22e961b3092cf0af0f
rhit-spiottdj <domspiotta@gmail.com>
2025-01-20 14:47:24 -0500

    Using spawn() to create separte processes #17
    Co-authored-by: kleinsv <kleinsv@users.noreply.github.com>

--- PythonTester/Mutator/MutationManager.py
+++ PythonTester/Mutator/MutationManager.py
@@ -13,8 +13,12 @@ def manageMutations(file_source, test_source):
     module_to_del = module_to_del[:-2].strip('.')
     if module_to_del in sys.modules:
         del sys.modules[module_to_del]
+    
+    ctx = multiprocessing.get_context("spawn")
     q = multiprocessing.Queue()
-    startupP = multiprocessing.Process(target=runMutationTest, args=(q, test_source,))
+    startupP = ctx.Process(target=runMutationTest, args=(q, parent + test_source))
+
+
     startupP.start()
     startupP.join()
     importlib.import_module(module_to_del)
@@ -22,7 +26,7 @@ def manageMutations(file_source, test_source):
     return result
 
 def runMutationTest(q, test_source):
-    test_suite = unittest.TestLoader().discover(parent + test_source, '*_test.py')
+    test_suite = unittest.TestLoader().discover(test_source, '*_test.py')
     runner = unittest.TextTestRunner()
     result = runner.run(test_suite)
     resultDict = {}
@@ -31,5 +35,4 @@ def runMutationTest(q, test_source):
     resultDict["unexpectedSuccesses"] = len(result.unexpectedSuccesses)
     resultDict["allPassed"] = result.wasSuccessful()
     resultDict["testsRun"] = result.testsRun
-
     q.put(resultDict)
